a9692be0cbde772b9d773cbe934ab53b38ce7e0b
Uday <58341470+im-usb@users.noreply.github.com>
2023-03-03 12:10:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,52 +1,45 @@
 function updateMonth() {
     let slider = document.getElementById("range-slider");
-    let bill = slider.value;
-    let rate = (bill * 0.16).toFixed(2);
+    let bill = document.getElementById("range-slider").value;
+    let rate = (bill*0.16).toFixed(2);
+
+    slider.addEventListener("input",updateMonth);
+
     document.getElementById("price").innerHTML = '$' + rate;
     document.getElementById("price-mob").innerHTML = '$' + rate;
-    document.getElementById("pageviews").innerHTML = bill + "K";
+    document.getElementById("pageviews").innerHTML = bill + "K"
 }
+
 
 function updateYear() {
     let slider = document.getElementById("range-slider");
-    let bill = slider.value;
-    let rate = (bill * 0.16 * 12).toFixed(2);
-    let finalRate = (rate - rate * 0.25).toFixed(2);
+    let bill = document.getElementById("range-slider").value;
+    let rate = (bill*0.16*12).toFixed(2);
+    let finalRate = (rate - rate*0.25).toFixed(2);
+
+    slider.addEventListener("input",updateYear);
+
     document.getElementById("price").innerHTML = '$' + finalRate;
     document.getElementById("price-mob").innerHTML = '$' + finalRate;
-    document.getElementById("pageviews").innerHTML = bill + "K";
+    document.getElementById("pageviews").innerHTML = bill + "K"
 }
 
-function billType() {
+function billType(){
     let btnValue = document.getElementById("toggle-btn").value;
-    if (btnValue == 2) {
+    let type = document.getElementById("toggle-btn");
+    type.addEventListener("input",billType);
+
+    if (btnValue == 2){
         document.getElementById("bill-type").innerHTML = '/year';
-        // Remove the event listener for the slider and add a new one for the updateYear() function
-        let slider = document.getElementById("range-slider");
-        slider.removeEventListener("input", updateMonth);
-        slider.addEventListener("input", updateYear);
-        updateYear();
-    } else if (btnValue == 1) {
+        document.getElementById("bill-type-mob").innerHTML = '/year';
+        updateYear();   
+    }
+    
+    else if(btnValue == 1){
         document.getElementById("bill-type").innerHTML = '/month';
-        // Remove the event listener for the slider and add a new one for the updateMonth() function
-        let slider = document.getElementById("range-slider");
-        slider.removeEventListener("input", updateYear);
-        slider.addEventListener("input", updateMonth);
+        document.getElementById("bill-type-mob").innerHTML = '/month';
         updateMonth();
     }
-}
-
-function addEventListeners() {
-    let slider = document.getElementById("range-slider");
-    let toggleBtn = document.getElementById("toggle-btn");
-
-    slider.addEventListener("input", function() {
-        updateMonth();
-    });
-
-    toggleBtn.addEventListener("input", function() {
-        billType();
-    });
 }
 
 var sliderColor = document.querySelector('.range-slider');
@@ -56,10 +49,9 @@ sliderColor.addEventListener('input', function() {
 });
 
 var toggleColor = document.querySelector('.toggle-btn');
-toggleColor.addEventListener('input', function() {
+toggleColor.addEventListener('input', function(){
     var value2 = (toggleColor.value - toggleColor.min) / (toggleColor.max - toggleColor.min) * 100;
     toggleColor.style.background = 'linear-gradient(to right, #10d8c4 0%, #10d8c4 ' + value2 + '%, #ddd ' + value2 + '%, #ddd 100%)';
 });
 
-addEventListeners();
 billType();
